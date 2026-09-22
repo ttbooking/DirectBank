7865d2d582b2c6a3b0501cdb3c5b5261019bf28e
Egor Gruzdev <egorgruzdev@gmail.com>
2026-09-22 22:54:51 +0200

    Код ошибки банка строкой, исходная ошибка доступна из ClientException
    ErrorType::getCode() возвращает string. ClientException::getBankCode()
    и getError() отдают код и ошибку из ответа банка как есть.
    
    Closes #9

--- src/Exceptions/ClientException.php
+++ src/Exceptions/ClientException.php
@@ -4,7 +4,33 @@ declare(strict_types=1);
 namespace TTBooking\DirectBank\Exceptions;
 
 
+use TTBooking\DirectBank\Objects\ErrorType;
+
 class ClientException extends \Exception
 {
+    protected ?ErrorType $error = null;
 
+    public static function fromError(ErrorType $error): static
+    {
+        $exception = new static($error->getDescription(), (int) $error->getCode());
+        $exception->error = $error;
+
+        return $exception;
+    }
+
+    /**
+     * Ошибка из ответа банка (ResultBank/Error), если она была
+     */
+    public function getError(): ?ErrorType
+    {
+        return $this->error;
+    }
+
+    /**
+     * Код ошибки банка как есть, строкой из 4 символов
+     */
+    public function getBankCode(): ?string
+    {
+        return $this->error?->getCode();
+    }
 }
