7108ecae8500d6a2c88f82d34aa10f0703968d59
Egor Gruzdev <egorgruzdev@gmail.com>
2026-09-22 22:54:13 +0200

    GetPackList: отметка времени в формате dd.MM.yyyy HH:mm:ss
    Closes #3

--- src/Dictionary/DefaultValue.php
+++ src/Dictionary/DefaultValue.php
@@ -7,4 +7,7 @@ namespace TTBooking\DirectBank\Dictionary;
 class DefaultValue
 {
     const FORMAT_VERSION = '2.2.2';
+
+    //Формат отметки времени в GetPackList: dd.MM.yyyy HH:mm:ss
+    const TIMESTAMP_FORMAT = 'd.m.Y H:i:s';
 }
